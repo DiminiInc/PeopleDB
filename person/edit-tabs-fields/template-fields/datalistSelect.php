349592d3c69e23640a43c2b7806b6ac7f5dc18d3
Dmitry Yaskovich <kadavr95@outlook.com>
2023-08-23 23:16:09 +0300

    Add option to set sql query for datalistSelect

--- person/edit-tabs-fields/template-fields/datalistSelect.php
+++ person/edit-tabs-fields/template-fields/datalistSelect.php
@@ -1,16 +1,17 @@
 <?php
 function datalistSelect($link, $name, $placeholder, $value, $table, $row,
-                        $value_function = null): void
+                        $value_function = null, $sql_query = null): void
 {
     $value_function ??= function ($value) {
         return $value;
     };
+    $sql_query ??= "SELECT DISTINCT `$value` FROM $table";
     ?>
     <input name="<?php echo $name ?>[]" placeholder="<?php echo $placeholder ?>"
            value="<?php echo $value_function($row[$value]) ?>" list="<?php echo $name ?>_list">
     <datalist id="<?php echo $name ?>_list">
         <?php
-        $result = mysqli_query($link, "SELECT DISTINCT `$value` FROM $table");
+        $result = mysqli_query($link, $sql_query);
         $row = mysqli_fetch_array($result);
         if (!is_null($row)) {
             do {
